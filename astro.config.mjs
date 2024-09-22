98e677bef030f17ec8da6bd048a84a8b42410ed0
iyyafi <iyyafi@gmail.com>
2024-09-22 22:11:47 +0700

    init astro, db, cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import db from '@astrojs/db';
+
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [db()],
+  output: 'server',
+  adapter: cloudflare()
+});
